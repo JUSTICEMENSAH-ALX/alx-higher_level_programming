a9e9796c43304383a807b28c332f7dd7d4f998cc
JUSTICEMENSAH <portjusto1@gmail.com>
2022-09-12 09:57:03 -0700

    TASK 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -12,7 +12,7 @@ def safe_function(fct, *args):
         Otherwise - the result of the call to fct.
     """
     try:
-        result = fct(*args)
+        return = fct(*args)
         return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
