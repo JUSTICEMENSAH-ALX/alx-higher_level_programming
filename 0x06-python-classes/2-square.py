22f2a2bd0416846acc0ecc17e9ccb4919b2351a9
JUSTICEMENSAH <portjusto1@gmail.com>
2022-09-13 09:46:07 -0700

    TASK 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creating a square class """
+
+
+class square
+   """ Defining a class square """
+   def __init__(self, size=0):
+       """ Initializing a square class
+       Args: size: size of the square
+       """
+
+       self.__size = size
