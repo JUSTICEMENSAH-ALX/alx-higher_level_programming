4f67827253a85ee0611f30ae31f9de877c8c861b
JUSTICEMENSAH-ALX <PORTJUSTO1@GMAIL.COM>
2023-01-17 16:29:28 +0000

    Higher Level Continues

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const filename = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        process.stdout.write(data);
-			      }
-});#!/usr/bin/node
-
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        process.stdout.write(data);
-			      }
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
